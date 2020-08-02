6ed006f928854612a0376afe8bc9b19da20cb145
Trevis McGill <trevismcgill25@gmail.com>
2020-08-02 22:00:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
 class Dog
+
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+
+  def name
+    @this_dog_name
+  end
+
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+
+  def breed
+    @this_dog_breed
 end
